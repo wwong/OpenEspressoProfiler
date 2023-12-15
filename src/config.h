a04d1ee6de03cd83c6acf481bdfb03287ec4344c
Wilson Wong <git@wwong.io>
2023-12-15 03:22:20 -0800

    Add support for XIAO ESP32S3

--- src/config.h
+++ src/config.h
@@ -1,8 +1,37 @@
 #ifndef OPENESPRESSOPROFILER_CONFIG_H
 #define OPENESPRESSOPROFILER_CONFIG_H
 
+#include <Arduino.h>
+
+#ifndef BUILD_ENV_NAME
+    #define generic_espwroom32 101
+#else
+    #define generic_espwroom32 101
+    #define xiao_esp32s3 102
+#endif
+
+#if !defined(BUILD_ENV_NAME)
+#warning "BUILD_ENV_NAME not set. Going to continue with generic configs."
 #define LED_PIN 2
+#define INVERT_LED false
 #define PRESSURE_PIN 32
+
+#elif BUILD_ENV_NAME==generic_espwroom32
+#pragma message "Using generic_espwroom32"
+#define LED_PIN 2
+#define INVERT_LED false
+#define PRESSURE_PIN 32
+
+#elif BUILD_ENV_NAME==xiao_esp32s3
+#pragma message "Using xiao_esp32s3"
+#define LED_PIN LED_BUILTIN
+#define INVERT_LED true
+#define PRESSURE_PIN A1
+
+#else
+#error "BUILD_ENV_NAME NOT RECOGNIZED"
+#endif
+
 
 #define R1_OHMS 10000
 #define R2_OHMS 15000
